72f0970ff56b73e1f82358e22ec8cbaf489b028a
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-09-07 03:38:42 +0900

    feat(Translations): update translation

---
+++ src/main/resources/youtube/translations/rename_translate.bat
@@ -0,0 +1,20 @@
+rename ar ar
+rename bg bg-rBG
+rename de de-rDE
+rename fr fr-rFR
+rename el el-rGR
+rename es-ES es-rES
+rename hu hu-rHU
+rename it it-rIT
+rename ja ja-rJP
+rename ko ko-rKR
+rename pl pl-rPL
+rename pt-BR pt-rBR
+rename ru ru-rRU
+rename tr tr-rTR
+rename uk uk-rUA
+rename vi vi-rVN
+rename zh-CN zh-rCN
+rename zh-TW zh-rTW
+
+::RD /S /Q bg
