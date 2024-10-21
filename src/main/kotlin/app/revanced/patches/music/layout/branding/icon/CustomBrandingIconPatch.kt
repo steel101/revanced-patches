115ff0d76db7aac59679eec221810bc3aaa3115f
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-10-21 20:27:31 +0900

    fix(YouTube Music/Custom branding icon): patch fails on certain versions

--- src/main/kotlin/app/revanced/patches/music/layout/branding/icon/CustomBrandingIconPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/layout/branding/icon/CustomBrandingIconPatch.kt
@@ -236,6 +236,9 @@ object CustomBrandingIconPatch : BaseResourcePatch(
         if (!SettingsPatch.upward0723) {
             return
         }
+        if (appIcon == "youtube_music") {
+            return
+        }
 
         fun getAdaptiveIconResourceFile(tag: String): String {
             context.xmlEditor["res/mipmap-anydpi/ic_launcher_release.xml"].use { editor ->
