e4dbf13608c7fe5296268f92dd9195cd80e53bf0
steel johnson <steelvargas@gmail.com>
2024-11-21 03:10:26 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
         url = uri("https://maven.pkg.github.com/revanced/multidexlib2")
         credentials {
             username = "steel101"
-            password =  project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            password =  ${{ secrets.GITHUB_TOKEN }} 
     }
 }
 
