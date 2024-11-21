46842a8f772eab4e2a27d486046cb64d5aae2df5
steel johnson <steelvargas@gmail.com>
2024-11-21 02:29:12 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
         url = uri("https://maven.pkg.github.com/revanced/multidexlib2")
         credentials {
             username = "steel101"
-            password =  project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            password =  project.findProperty("gpr.key") as String? ?: System.getenv("GH_TOKEN")
     }
 }
 
@@ -104,7 +104,7 @@ publishing {
             url = uri("https://maven.pkg.github.com/steel101/revanced-patches")
             credentials {
                 username = "steel101"
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GH_TOKEN")
             }
         }
     }
