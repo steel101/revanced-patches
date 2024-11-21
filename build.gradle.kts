b03b1f28a299be75db3e21fceb1f2603a303d274
steel johnson <steelvargas@gmail.com>
2024-11-21 04:36:59 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ repositories {
         credentials {
             username = "steel101"
             println(username)
-            pass = project.findProperty("gpr.key") as String? 
-            println(pass)
+            password = project.findProperty("gpr.key") as String? 
+            println(password)
             password = project.findProperty("gpr.key") as String? ?: System.getenv("GH_TOKEN")
             println(password)
           }
